e1bff0cf243aa2f7edc452a1a9f38271dd39a58f
Rajasekar <erajasekargv@gmail.com>
2025-04-27 22:41:21 +0530

    excel-reader: upload file input added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'excel-reader';
+  public title = 'excel-reader';
+  public fileName= '';
+  public requiredFileType = 'application/pdf';
+  public selectedFile: File | null = null;
+
+  public onFileSelected(event: Event): void {
+    const fileInput: HTMLInputElement = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedFile = fileInput.files[0];
+      this.fileName = this.selectedFile.name;
+    }
+  }
+
+  public onUpload(): void {
+    if (!this.selectedFile) {
+      return;
+    }
+
+  }
 }
